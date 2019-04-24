9348b65021988fe21ca963a3e1a8614688c8eb8e
Nguyen Manh Hung <hungnmgch15283@fpt.edu.vn>
2019-04-24 09:11:03 +0700

    First Commit

---
+++ Flappy Bird Remake/Assets/Scripts/Controller/GameManagement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagement : MonoBehaviour
+{
+
+    public static GameManagement instance;
+
+    private const string HIGH_SCORE = "High Score";
+
+     void Awake()
+    {
+        _MakeSingleInstance();
+        IsGameStartForTheFirstTime();
+    }
+
+    void IsGameStartForTheFirstTime()
+    {
+        if (!PlayerPrefs.HasKey("IsGameStartForTheFirstTime")){
+            PlayerPrefs.SetInt(HIGH_SCORE, 0);
+            PlayerPrefs.SetInt("IsGameStartForTheFirstTime",0);
+        }
+    }
+
+    void _MakeSingleInstance()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void _SetHighScore(int score)
+    {
+        PlayerPrefs.SetInt(HIGH_SCORE, score);
+    }
+
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt(HIGH_SCORE);
+    }
+}
---
+++ Flappy Bird Remake/Assets/Scripts/Controller/GamePlayController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GamePlayController : MonoBehaviour
+{
+
+    public static GamePlayController instance;
+
+    [SerializeField]
+    private Button instructionButton;
+
+    [SerializeField]
+    private Text scoreText, endScoreText, bestScoreText;
+
+    [SerializeField]
+    private GameObject gameOverPanel, pausePanel;
+
+     void Awake()
+    {
+        Time.timeScale = 0;
+        _MakeInstance();
+    }
+    void _MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void InstructionButton()
+    {
+        Time.timeScale = 1;
+        instructionButton.gameObject.SetActive(false);
+    }
+
+    public void _SetScore(int score)
+    {
+        scoreText.text = "" + score;
+    }
+    public void _BirdDiedShowPanel(int score)
+    {
+        gameOverPanel.SetActive(true);
+        endScoreText.text = "" + score;
+        if (score > GameManagement.instance.GetHighScore())
+        {
+            GameManagement.instance._SetHighScore(score);
+        }
+        bestScoreText.text = "" + GameManagement.instance.GetHighScore();
+    }
+
+    public void _MenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void _RestartButton()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void _RestartButtonGamePlay2()
+    {
+        SceneManager.LoadScene("GamePlay2");
+    }
+
+    public void _PauseButton()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void _ResumeButton()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
